49dddea89cc1870700361165797d5233c4c6b10c
scragly <29337040+scragly@users.noreply.github.com>
2018-07-13 22:21:16 +1000

    add partial matching

--- manibot/utils/fuzzymatch.py
+++ manibot/utils/fuzzymatch.py
@@ -14,6 +14,28 @@ def get_match(word_list: list, word: str, score_cutoff: int = 60):
         return (None, None)
     return result
 
+def get_partial_match(word_list: list, word: str, score_cutoff: int = 60):
+    """Uses fuzzywuzzy to see if word is close to entries in word_list
+
+    Returns a tuple of (MATCH, SCORE)
+    """
+    result = process.extractOne(
+        word, word_list, scorer=fuzz.partial_ratio, score_cutoff=score_cutoff)
+    if not result:
+        return (None, None)
+    return result
+
+def find_matches(word_list: list, word: str, score_cutoff: int = 60):
+    """Uses fuzzywuzzy to see if word is close to entries in word_list
+
+    Returns a tuple of (MATCH, SCORE)
+    """
+    result = process.extractBests(
+        word, word_list, scorer=fuzz.ratio, score_cutoff=score_cutoff)
+    if not result:
+        return []
+    return result
+
 class FuzzyEnum(Enum):
     """Enumeration with fuzzy-matching classmethods."""
 
